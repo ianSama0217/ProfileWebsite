d46e68ed4f6e969e828db5efbf845737c726d557
ianSama0217 <ian20000217@gmail.com>
2023-10-16 23:30:01 +0800

    complate ErrorView

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <h1>錯誤404</h1>
+  <h1>網址輸入錯誤，請確認輸入的網址是否正確</h1>
+  <img src="../../public/aboutImg/giwawa-cover.jpg" class="errorImg" />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h1 {
+  font-size: 3rem;
+  margin: 1rem 0;
+  text-align: center;
+}
+.errorImg {
+  width: 80%;
+  border-radius: 8px;
+  margin: 1rem 10%;
+}
+</style>
