1cf36054ff92f138bb0537b8e3fb8e7ebc729d9d
ianSama0217 <ian20000217@gmail.com>
2023-10-16 12:23:38 +0800

    project main page 90%

---
+++ src/components/projectCrad.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  img: {
+    src: { type: String, default: "" },
+    height: { type: Number, default: 200 },
+    width: { type: Number, default: 200 },
+    alt: { type: String, default: "網站作品" },
+    title: { type: String, default: "網站作品" },
+  },
+  projectTitle: {
+    type: String,
+    default: "網頁設計作品",
+  },
+  link: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div class="projectCard">
+    <div class="imgBox">
+      <img :src="props.img.src" :alt="props.img.alt" :title="props.img.title" />
+    </div>
+
+    <div class="titleArea">
+      <h3>{{ props.projectTitle }}</h3>
+      <RouterLink :to="props.link" class="link">詳細資訊</RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.projectCard {
+  border: 2px solid #232323;
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 8px;
+  background-color: #b3b3b3;
+  margin: 1rem 0;
+  .imgBox {
+    padding: 0.5rem;
+    margin: 1rem 0;
+    border: 2px solid #858484;
+    border-radius: 8px;
+    overflow: hidden;
+
+    img {
+      height: 200px;
+      width: 200px;
+      border-radius: 8px;
+      transition: all 0.5s ease;
+
+      &:hover {
+        transform: scale(1.2);
+      }
+    }
+  }
+
+  .titleArea {
+    background-color: #b3b3b3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h3 {
+      font-size: 2rem;
+      background-color: #b3b3b3;
+      margin-bottom: 1rem;
+    }
+    .link {
+      font-size: 1.5rem;
+      padding: 0.5rem;
+      border-radius: 8px;
+      text-decoration: none;
+      background-color: #b3b3b3;
+      border: 2px solid #777;
+      margin-bottom: 1rem;
+      transition: all 0.3s ease;
+
+      &:hover {
+        background-color: #777;
+        color: white;
+      }
+    }
+  }
+}
+</style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,7 +1,86 @@
-<script setup></script>
+<script setup>
+import projectCrad from "../components/projectCrad.vue";
+</script>
 
 <template>
-  <h1>作品</h1>
+  <h1>我的作品集</h1>
+
+  <div class="projectArea">
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/expense-tracker.png`,
+        alt: `記帳網站`,
+        title: `記帳網站`,
+      }"
+      projectTitle="記帳網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/dessert.png`,
+        alt: `甜點網站`,
+        title: `甜點網站`,
+      }"
+      projectTitle="甜點網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/saleHouse.png`,
+        alt: `房屋網站`,
+        title: `房屋網站`,
+      }"
+      projectTitle="房屋網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/GPA-colculation.jpg`,
+        alt: `成績計算網站`,
+        title: `成績計算網站`,
+      }"
+      projectTitle="成績計算網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/japanFujiYama.jpg`,
+        alt: `日本旅遊網站`,
+        title: `日本旅遊網站`,
+      }"
+      projectTitle="日本旅遊網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/ramen.jpg`,
+        alt: `ラーメン屋網站`,
+        title: `ラーメン屋網站`,
+      }"
+      projectTitle="ラーメン屋網站"
+    />
+
+    <projectCrad
+      :img="{
+        src: `../../public/projectImg/weatherAPI.png`,
+        alt: `天氣預報網站`,
+        title: `天氣預報網站`,
+      }"
+      projectTitle="天氣預報網站"
+    />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h1 {
+  font-size: 3rem;
+  text-align: center;
+  margin: 1rem 0;
+}
+.projectArea {
+  padding: 0 10%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>
