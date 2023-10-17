3a541f08161cf9dbd412dbaa3f1bcc40f28ccf89
ianSama0217 <ian20000217@gmail.com>
2023-10-17 09:18:43 +0800

    complete buildNote

---
+++ src/views/noteChildren/BuildNoteView.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="buildArea">
+    <h1>網頁建置中...</h1>
+    <img
+      src="../../../public/builderImg/builder.gif"
+      class="buildImg"
+      alt="building..."
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.buildArea {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  .buildImg {
+    width: 80%;
+    border-radius: 8px;
+    margin: 1rem 10%;
+  }
+}
+</style>
