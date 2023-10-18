b431b487edbcf3eb5b91e41f7c184682a5c9ef45
ianSama0217 <ian20000217@gmail.com>
2023-10-18 10:25:45 +0800

    debug (remove scoller)

--- src/components/singleProjectComponent/webiframe.vue
+++ src/components/singleProjectComponent/webiframe.vue
@@ -19,26 +19,6 @@ const props = defineProps({
   border-radius: 8px;
   width: 60vw;
   height: 50vh;
-  overflow-y: scroll;
-
-  /* 修改滚动条的宽度和颜色 */
-  &::-webkit-scrollbar {
-    width: 15px; /* 修改滚动条的宽度 */
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: #b3b3b3; /* 修改滚动条滑块的颜色 */
-  }
-
-  /* 修改滚动轨道的颜色 */
-  &::-webkit-scrollbar-track {
-    background-color: #d9d9d9; /* 修改滚动轨道的颜色 */
-  }
-
-  /* 隐藏滚动按钮 */
-  &::-webkit-scrollbar-button:single-button {
-    display: none; /* 隐藏滚动按钮 */
-  }
 
   .webImg {
     width: 100%;
