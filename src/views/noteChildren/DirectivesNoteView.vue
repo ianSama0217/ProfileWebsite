678b4320f1af5e6f57544d4780b4df3574c2754d
ianSama0217 <ian20000217@gmail.com>
2023-10-19 17:36:43 +0800

    add NoteView children router(not link index.js)

---
+++ src/views/noteChildren/DirectivesNoteView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>template指令子路徑</h1>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/noteChildren/LifeCycleNoteView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>生命週期子路徑</h1>
+</template>
+
+<style scoped lang="scss"></style>
