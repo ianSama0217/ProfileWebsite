673078bae59cf2f8170ab5754b587834264ab2d9
ianSama0217 <ian20000217@gmail.com>
2023-10-19 17:34:25 +0800

    add note Card for NoteView

---
+++ src/components/noteCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink } from "vue-router";
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Vue語法",
+  },
+  link: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div class="noteLinkBox">
+    <h3 class="noteTitle">{{ title }}</h3>
+    <RouterLink class="noteLink" :to="item" v-for="item in link">{{
+      item
+    }}</RouterLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.noteLinkBox {
+  border-radius: 8px;
+  border: 2px solid #777;
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0.5rem;
+  margin: 1rem;
+  .noteTitle {
+    font-size: 1.8rem;
+    align-self: center;
+    margin-bottom: 0.5rem;
+  }
+  .noteLink {
+    color: grey;
+    text-decoration: none;
+    font-size: 1.5rem;
+    margin: 0.2rem;
+    transition: all 0.5s ease;
+
+    &:hover {
+      background-color: #b3b3b3;
+    }
+  }
+}
+</style>
--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -1,9 +1,88 @@
 <script setup>
+import { ref, reactive } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import noteCard from "../components/noteCard.vue";
+
+const DirectivesTitle = ref("template語法");
+//router to=""的值 ，也是標籤內名稱
+const DirectivesLink = ref([
+  "v-text",
+  "v-html",
+  "v-if",
+  "v-else-if",
+  "v-else",
+  "v-for",
+  "v-on",
+  "v-model",
+  "v-bind",
+  "v-model",
+  "v-slot",
+]);
+
+const RouterDocTitle = ref("Router");
+//router to=""的值 ，也是標籤內名稱
+const RouterDocLink = ref(["About router", "RouterLink", "Dynamic routing"]);
+
+const LifecycleTitle = ref("Life Cycle");
+//router to=""的值 ，也是標籤內名稱
+const LifecycleLink = ref([
+  "About life cycle",
+  "onMounted()",
+  "onUpdated()",
+  "onUnmounted()",
+  "onBeforeMount()",
+  "onBeforeUpdate()",
+  "onBeforeUnmount()",
+]);
+
+const CompositionAPITitle = ref("Composition API");
+//router to=""的值 ，也是標籤內名稱
+const CompositionAPILink = ref([
+  "ref()",
+  "reactive()",
+  "computed()",
+  "readonly()",
+  "watch()",
+]);
+
+const PiniaTitle = ref("Pinia");
+//router to=""的值 ，也是標籤內名稱
+const PiniaLink = ref(["About Pinia"]);
+
+// const linkURL = computed();
 </script>
 
 <template>
-  <h1>vue筆記</h1>
+  <!-- <img src="../../public/noteImg/vuejs.svg" alt="vue-logo" class="logoImg" /> -->
+  <div class="waterfallflow">
+    <!-- template語法box -->
+    <noteCard
+      :title="DirectivesTitle"
+      :link="DirectivesLink"
+      class="noteCard"
+    />
+    <!-- Router語法box -->
+    <noteCard :title="RouterDocTitle" :link="RouterDocLink" class="noteCard" />
+    <!-- Lifecycle語法box -->
+    <noteCard :title="LifecycleTitle" :link="LifecycleLink" class="noteCard" />
+    <!-- CompositionAPI語法box -->
+    <noteCard
+      :title="CompositionAPITitle"
+      :link="CompositionAPILink"
+      class="noteCard"
+    />
+    <!-- Pinia語法box -->
+    <noteCard :title="PiniaTitle" :link="PiniaLink" class="noteCard" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.waterfallflow {
+  padding: 5% 10%;
+  // 將欄分成3等分
+  column-count: 3;
+  .noteCard {
+    break-inside: avoid; //防止noteCard被分割
+  }
+}
+</style>
