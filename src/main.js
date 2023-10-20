757dd1d36de12db2c71fff934f9ad6d6d1609430
ianSama0217 <ian20000217@gmail.com>
2023-10-20 16:11:18 +0800

    import vue3-prism

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+//匯入vue3-prism套件(code-board要用的)
+import Vue3Prism from "vue3-prism/lib/Vue3Prism.common.js";
+import "vue3-prism/lib/Vue3Prism.css";
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(Vue3Prism);
 
 app.mount("#app");
