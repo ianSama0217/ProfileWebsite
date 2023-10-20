2e9ce3ed4e6a17419b7e60be5ae151c466aec982
ianSama0217 <ian20000217@gmail.com>
2023-10-21 01:04:33 +0800

    add noteView's children router (Component)

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import DirectivesNoteView from "../views/noteChildren/DirectivesNoteView.vue";
 import RouterNoteView from "../views/noteChildren/RouterNoteView.vue";
 import LifeCycleNoteView from "../views/noteChildren/LifeCycleNoteView.vue";
 import CompositionAPINoteView from "../views/noteChildren/ReactivityCoreNoteView.vue";
+import ComponentNoteView from "../views/noteChildren/CompontentNoteView.vue";
 import PiniaNoteView from "../views/noteChildren/PiniaNoteView.vue";
 
 const router = createRouter({
@@ -44,6 +45,10 @@ const router = createRouter({
         {
           path: "CompositionAPI/:domain",
           component: CompositionAPINoteView,
+        },
+        {
+          path: "Compontent/:domain",
+          component: ComponentNoteView,
         },
         {
           path: "Pinia/:domain",
